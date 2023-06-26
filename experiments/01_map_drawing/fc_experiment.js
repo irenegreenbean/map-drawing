89a31f222ae8787dfe1bad35afcbfd27bac6c0af
irenegreenbean <ireneyi@berkeley.edu>
2023-06-26 11:20:41 -0400

    Revert "Update fc_experiment.js"
    This reverts commit f4c239083d7dae7a466418f4594426a016bdb7ef.

--- experiments/01_map_drawing/fc_experiment.js
+++ experiments/01_map_drawing/fc_experiment.js
@@ -112,11 +112,9 @@ const label = {
     return `<img src="${imageData}"></img>`;
   },
   questions: [
-    {prompt: '请列出您画出来所有区域的标签。'},
-    required: true,
+    {prompt: '请列出您画出来所有区域的标签。'}
   ],
-  button_label: '继续',
-  
+  required: true,
 }
 
 timeline.push(map);
