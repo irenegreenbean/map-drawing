ea7c2c47b4cb09eef3deebaed4d87df4f04a8780
irenegreenbean <ireneyi@berkeley.edu>
2023-07-20 17:09:32 -0400

    Update fc_experiment.js

--- experiments/01_free_classification/fc_experiment.js
+++ experiments/01_free_classification/fc_experiment.js
@@ -85,7 +85,7 @@ timeline.push(instructions);
 const map = {
 
   type: jsPsychSketchpad,
-  prompt: '<p style="width:380px">请在以下的地图上画圈，显示您认为人们说普通话方式不同的地方。您所画的区域可以数量不限，大小不限，可以重叠。</p>',
+  prompt: '<p style="width:380px">您知道有哪些方言，请在地图上标明他们的位置及方言名称（您先画完区域以后再按“全画完了”，就可以列出您所画的方言名称了）。您所画的区域可以数量不限，大小不限，可以重叠。</p>',
   prompt_location: 'abovecanvas',
   stroke_color_palette: ['red'],
   stroke_color: 'red',
@@ -104,6 +104,8 @@ const map = {
   save_final_image: true,
   on_finish: function(data) {
     console.log(data)
+
+    // 请在以下的地图上画圈，显示您认为人们说普通话方式不同的地方。
   }
 
 
